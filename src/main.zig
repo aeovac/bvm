e83b15fe8193f17dbc79faef3fb15c16dcac7d88
aeovac <predam@gmx.com>
2025-03-27 20:26:01 +0100

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,42 +1,30 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const http = std.http;
-const Os = builtin.os;
-const Arch = @import("builtin").cpu.arch;
 
 const print = std.debug.print;
 
 fn get_bun_releases() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const client = std.http.Client{ .allocator = gpa.allocator() };
-    const request = try client.open(.{
+    defer _ = gpa.deinit();
+
+    var client = std.http.Client{ .allocator = gpa.allocator() };
+    defer client.deinit();
+
+    var body = std.ArrayList(u8).init(gpa.allocator());
+    defer body.deinit();
+
+    _ = try client.fetch(.{
         .method = .GET,
-        .uri = std.Uri.parse("https://github.com/oven-sh/bun/releases")
+        .location = .{
+            .url = "https://github.com/oven-sh/bun/releases"
+        },
+        .server_header_buffer = undefined,
+        .response_storage = .{ .dynamic =  &body }
     });
-    defer request.deinit();
 
-
-    print(request.response.content_disposition, .{});
+    print("Response: {s}\n", .{body.items});
 }
-
-pub fn get_architecture() ![]const u8  {
-    const os = 
-        if (Os.tag == .windows) "windows" 
-        else if (Os.tag == .linux) "linux"
-        else if (Os.tag.isDarwin()) "darwin";
-
-    const arch = switch (Arch) {
-        .x86_64 => "x64",
-        .arm => "aarch64",
-        else => @tagName(Arch),
-    };
-
-    return try std.fmt.allocPrint(std.heap.page_allocator, "{s}-{s}", .{os, arch});
-}
-
 
 pub fn main() !void {
-    std.debug.print("test", .{});
-    
-    try get_bun_tags();
+    try get_bun_releases();
 }
